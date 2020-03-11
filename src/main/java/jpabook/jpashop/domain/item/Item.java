8812ce5a14838b616af0d723276ce5950624cd1a
sooooojinlee <soooojin.lee@gmail.com>
2020-03-12 08:42:13 +0900

    상품 비즈니스 로직 추가, 리포지토리, 서비스

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,15 +1,17 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter @Service
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -24,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직
+
+    //stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
