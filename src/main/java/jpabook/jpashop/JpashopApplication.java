d03f2096ad367f59d1a8db7ca127fa5f714189ab
sooooojinlee <soooojin.lee@gmail.com>
2020-03-11 21:35:49 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.support.SimpleTriggerContext;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
